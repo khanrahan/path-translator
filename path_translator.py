f0634447ab8b24331e68104103922a08dcce193c
Kieran Hanrahan <kieran@knhn.me>
2025-09-05 13:59:22 -0400

    Bump version

--- path_translator.py
+++ path_translator.py
@@ -2,7 +2,7 @@ r"""
 Script Name: Path Translator
 Written By: Kieran Hanrahan
 
-Script Version: 2.0.0
+Script Version: 2.1.0
 Flame Version: 2025
 
 URL: http://www.github.com/khanrahan/path-translator
@@ -47,7 +47,7 @@ import flame
 from PySide6 import QtCore, QtGui, QtWidgets
 
 TITLE = 'Path Translator'
-VERSION_INFO = (2, 0, 0)
+VERSION_INFO = (2, 1, 0)
 VERSION = '.'.join([str(num) for num in VERSION_INFO])
 TITLE_VERSION = f'{TITLE} v{VERSION}'
 MESSAGE_PREFIX = '[PYTHON]'
